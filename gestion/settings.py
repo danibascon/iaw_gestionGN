298044c2770987adf3b7af9efaaf0aa238eb2341
dani <bascon1991@hotmail.es>
2017-12-01 09:42:31 +0100

    cambios

--- gestion/settings.py
+++ gestion/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'gestion.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'danibascon$default',
+	'USER': 'danibascon',
+	'PASSWORD': 'bascon1234.',
+	'HOST': 'danibascon.mysql.pythonanywhere-services.com',
+	'PORT': '',
     }
 }
 
--- gestion/wsgi.py
+++ gestion/wsgi.py
@@ -10,6 +10,10 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
+import sys
+sys.path.insert(0, '/home/dani/Documentos/practica-Imp/iaw_gestionGN')
+from aplicacion.app import app as application
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gestion.settings")
 
 
